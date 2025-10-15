1987568014794f7c41f10247f613bbeb551aeee1
Topias Perälä <topias.perala1@gmail.com>
2025-10-16 00:22:13 +0300

    navbar fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -78,10 +78,8 @@ export default function Navbar() {
         <Link to="/groups" className="navbar-link">Groups</Link>
         <Link to="/browse" className="navbar-link">Browse</Link>
 
-        {!isLoggedIn ? (
+        {!isLoggedIn && (
           <Link to="/login" className="navbar-link desktop-login">Login</Link>
-        ) : (
-          <button onClick={handleLogout} className="logout-button">Logout</button>
         )}
         
         <div className={`navbar-menu-wrapper ${!isLoggedIn ? 'hide-on-desktop' : ''}`}>
